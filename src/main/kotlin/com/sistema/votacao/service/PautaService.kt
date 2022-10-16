d27265a88b594045aada95e99c24669c8cf66f2c
estherwl <estherlucena@gmail.com>
2022-10-16 15:45:01 -0300

    adiciona log na classe PautaService

--- src/main/kotlin/com/sistema/votacao/service/PautaService.kt
+++ src/main/kotlin/com/sistema/votacao/service/PautaService.kt
@@ -1,23 +1,22 @@
 package com.sistema.votacao.service
 
+import com.sistema.votacao.controller.dto.PautaDTO
 import com.sistema.votacao.exception.BusinessException
 import com.sistema.votacao.model.Pauta
 import com.sistema.votacao.model.Resultado
 import com.sistema.votacao.model.Voto
 import com.sistema.votacao.repository.PautaRepository
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
 @Service
 class PautaService(private val pautaRepository: PautaRepository) {
-    val TEMPO_PADRAO:Long = 1
+    val TEMPO_PADRAO: Long = 1
 
     fun cadastraPauta(titulo: String, encerramentoVotacao: Long?): Pauta {
         val duracaoVotacao = encerramentoVotacao ?: TEMPO_PADRAO
-
-        val pauta = Pauta(null, titulo, true, null, null, duracaoVotacao, verificaDataEncerramento(
-            duracaoVotacao
-        ))
+        val pauta = Pauta(null, titulo, true, null, null, duracaoVotacao, verificaDataEncerramento(duracaoVotacao))
         return pautaRepository.save(pauta)
     }
 
@@ -29,19 +28,22 @@ class PautaService(private val pautaRepository: PautaRepository) {
         return pautaRepository.findAll()
     }
 
-    fun salvaVoto(pauta: Pauta, voto: Voto){
+    fun salvaVoto(pauta: Pauta, voto: Voto) {
         pautaRepository.save(pauta.apply { votos = this.votos?.plus(voto) })
     }
 
     fun verificaVotacaoEstaEncerrada(pauta: Pauta) {
         if (pauta.dataEncerramento.isBefore(LocalDateTime.now())) {
+            logger.info("Encerrando votação")
             contaResultado(pauta)
             pautaRepository.save(pauta.apply { estaAberta = false })
             throw BusinessException("Pauta encerrada para votação")
-       }
+        }
     }
 
     private fun contaResultado(pauta: Pauta) {
+        logger.info("Apurando resultado")
+
         val votosSim = pauta.votos!!.count { it.voto }
         val votosNao = pauta.votos!!.count { !it.voto }
 
@@ -54,6 +56,10 @@ class PautaService(private val pautaRepository: PautaRepository) {
 
     private fun verificaDataEncerramento(encerramentoVotacao: Long): LocalDateTime {
         return LocalDateTime.now().plusMinutes(encerramentoVotacao)
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java)
     }
 
 }
